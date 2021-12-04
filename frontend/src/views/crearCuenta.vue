12130e2cd672d773a0a0c5954c75e0857ffadd2b
Daniel Zuñiga <1danielarmando.1999@gmail.com>
2021-12-04 15:29:43 -0500

    CrearCuenta actualizado

--- frontend/src/views/crearCuenta.vue
+++ frontend/src/views/crearCuenta.vue
@@ -6,8 +6,8 @@
         </div>
         <div class="container">
             <div>
-                <input type="text" v-model="nombres" placeholder="Nombres">
-                <input type="text" v-model="apellidos" placeholder="Apellidos">
+                <input type="text" v-model="nombres" placeholder="Nombres" maxlength="50">
+                <input type="text" v-model="apellidos" placeholder="Apellidos" maxlength="50">
             </div>
             <div>
                 <input type="text" v-model="email" placeholder="Correo electrónico">
@@ -16,7 +16,7 @@
                 <input type="text" v-model="new_user" placeholder="Nombre usuario nuevo">
             </div>
             <div>
-                <input type="password" v-model="password" placeholder="Contraseña nueva">
+                <input type="password" v-model="password" placeholder="Contraseña nueva" minlength="8" maxlength="20">
             </div>
             <div>
                 <form>
@@ -25,21 +25,38 @@
                 </form>
             </div>
             <div>
-                <div><label>Genero:</label></div>
+                <div><label>Genero y orientación sexual:</label></div>
                 <div>
                     <select v-model="gender">
-                        <option disabled value=""></option>
+                        <option>Hombre cisgenero</option>
+                        <option>Mujer cisgenero</option>
+                        <option>Hombre transgenero</option>
+                        <option>Mujer transgenero</option>
                         <option>Hombre</option>
                         <option>Mujer</option>
-                        <option>Otros</option>
-                        
+                        <option>Persona no binaria</option>
+                        <option>Genero fluido</option>
+                        <option>Intersexual</option>
+                        <option>Otro</option>
+                        <option>Prefiero no contestar</option>
+                    </select>
+                    <select v-model="orientacion">
+                        <option>Heterosexual</option>
+                        <option>Homosexual</option>
+                        <option>Bisexual</option>
+                        <option>Pansexual</option>
+                        <option>Asexual</option>
+                        <option>Antrosexual</option>
+                        <option>Demisexual</option>
+                        <option>Otro</option>
+                        <option>Prefiero no contestar</option>
                     </select>
                 </div>
             </div>
             <div>
-                <button>Crear cuenta</button>
+                <button v-on:click="checked">Crear cuenta</button>
             </div>
-            {{nombres}} - {{apellidos}} - {{email}} - {{new_user}} - {{password}} - {{born}} - {{gender}}
+            {{nombres}} - {{apellidos}} - {{email}} - {{new_user}} - {{password}} - {{born}} - {{gender}} - {{orientacion}}
         </div>
     </div>
    </template>
@@ -55,16 +72,25 @@
                     new_user: "",
                     password: "",
                     born: new Date().toISOString().substr(0, 10),
+                    today: new Date(),
                     gender: "",
+                    orientacion: "",
                 }
             },
-            mounted() {
-
-            },
             methods: {
+                checked(){
 
+                    if(!this.email.includes("@gmail.com")){
+                        alert("El correo indicado no es gmail")
+                    }else if(this.password.length < 8){
+                        alert("La contraseña debe tener más de 8 caracteres")
+                    }
+                    
+                }
             }
         }
 
         
     </script>
+
+
