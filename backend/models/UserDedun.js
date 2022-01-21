eea3a319d576db05fa10b0127c032ac345ea39b1
Juan David <judgonzalezmu@unal.edu.co>
2022-01-21 11:40:19 -0500

    Guardar datos de registro
    Ahora guarda todos los campos del registro en dedunDB

--- backend/models/UserDedun.js
+++ backend/models/UserDedun.js
@@ -1,10 +1,18 @@
 //Cremamos la tabla User utilizando sequelize en la base de datos
 
 module.exports = (sequelize, DataTypes) =>
-  sequelize.define('User', {
+  sequelize.define('User', {    
     email: {
       type: DataTypes.STRING,
       unique: true
     },
-    password: DataTypes.STRING
+    name: DataTypes.STRING,
+    nick: DataTypes.STRING,
+    password: DataTypes.STRING,
+    birth: DataTypes.STRING,
+    gender: DataTypes.STRING,
+    newgender: DataTypes.STRING,
+    orientation: DataTypes.STRING,
+    neworientation: DataTypes.STRING
   })
+
