1f3f64f52bc6df6fa1f8b7c8ec31bfb553e7db60
Alejandro Chávez <cchavezb@unal.edu.co>
2022-01-24 15:22:39 -0500

    script working better

--- backend/controllers/ScriptsController.js
+++ backend/controllers/ScriptsController.js
@@ -4,16 +4,26 @@ module.exports = {
   async bigFiveP (req, res) {
     console.log('We got to the backend')
     try {
-      const pythonScript = spawn('python', ['test.py'])
-      console.log('the Spawn thing was done')
-      pythonScript.stdout.on('data', function (data) {
-        console.log('Everything was done')
-        alert('Guardado')
-        alert(data.toString())
+      const pythonScript = spawn('python', ['computeBigFiveFactors.py', 't', 8])
+      pythonScript.stderr.on('data', (data) => {
+        console.error(`child stderr:\n${data}`)
       })
-      res.send('hola')
+      // res.send(pythonScript)
+      console.log(pythonScript)
+      console.log('the Spawn thing was done')
+      console.log('the New ON')
+      var retrievedData
+      pythonScript.stdout.on('data', function (data) {
+        console.log('Data from python script')
+        retrievedData = data.toString()
+      })
+      pythonScript.on('close', (code) => {
+        console.log('closing child process')
+        res.send(retrievedData)
+      })
+      // res.send('hola')
     } catch (err) {
-      alert('Error')
+      console.log('Error pero del try')
       res.status(400).send({
         error: 'We could not'
       })
